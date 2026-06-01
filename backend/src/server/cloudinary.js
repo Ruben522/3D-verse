7f2b06b209889b368d19a67e87bd04c0865723b6
rubenm <rubiosax52@gmail.com>
2026-06-01 18:17:29 +0200

    Probando el servicio de Cloud para el almacenamiento de imágenes y modelos 3.1

--- backend/src/server/cloudinary.js
+++ backend/src/server/cloudinary.js
@@ -1,7 +1,8 @@
 import { v2 as cloudinary } from 'cloudinary';
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import pkg from 'multer-storage-cloudinary';
 import multer from 'multer';
 import 'dotenv/config';
+const { CloudinaryStorage } = pkg;
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
